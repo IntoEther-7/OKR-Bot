b2b90ae3b77d857771b34260e06a4f07500b189b
Ether <1779562376@qq.com>
2023-08-17 11:23:42 +0800

    正确设置了缓存

--- src/main/java/com/hellocrop/okrbot/util/DocumentCheckUtil.java
+++ src/main/java/com/hellocrop/okrbot/util/DocumentCheckUtil.java
@@ -1,5 +1,7 @@
 package com.hellocrop.okrbot.util;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -7,6 +9,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 
+@Slf4j
 public class DocumentCheckUtil {
 
     private final Properties properties;
@@ -16,8 +19,18 @@ public class DocumentCheckUtil {
         properties = new Properties();
         try {
             File file = new File("src/main/resources/DocInfo/document.properties");
-            if (!file.getParentFile().exists()) file.getParentFile().mkdirs();
-            if (!file.exists()) file.createNewFile();
+            if (!file.getParentFile().exists()) {
+                log.info("文件夹不存在，已创建");
+                file.getParentFile().mkdirs();
+            } else {
+                log.info("文件夹存在");
+            }
+            if (!file.exists()) {
+                log.info("文档信息文件不存在，已创建");
+                file.createNewFile();
+            } else {
+                log.info("文档信息文件存在");
+            }
             properties.load(new FileReader(file, StandardCharsets.UTF_8));
         } catch (IOException e) {
             throw new RuntimeException(e);
